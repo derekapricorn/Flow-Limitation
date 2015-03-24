b6dd1dc740dffd346f52262f5fffb23a5397d4fc
Derek Zhi <derek.zhi@mail.utoronto.ca>
2015-03-24 12:00:27 -0400

    timetable.m

--- code/anno_analysis/timetable.m
+++ code/anno_analysis/timetable.m
@@ -1,11 +1,15 @@
 function [] = timetable ()
+%this function looks at all the intervals of the tp.mat files and
+%genereates a timetable txt file to contain all the intervals
 filedir = uigetdir;
 cd(filedir);
 fname = dir('*.mat');
 list = [];
 for jj = 1:length(fname)
-    load(fname(jj).name);
-    list = [list;int16(t(1)),int16(t(end))];
+    if ~strcmp(fname(jj).name,'event_time.mat')
+        load(fname(jj).name);
+        list = [list;int16(t(1)),int16(t(end))];
+    end
 end
 
 T = table(list);

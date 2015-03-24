b3079ffd6c0894bdebdda3de481ca39cf1576641
Derek Zhi <derek.zhi@mail.utoronto.ca>
2015-03-24 12:18:23 -0400

    anno_analysis
    added low signal category

--- code/anno_analysis/anno_analysis.m
+++ code/anno_analysis/anno_analysis.m
@@ -5,9 +5,9 @@ filedir = uigetdir;
 cd(filedir);
 load('event_time.mat')
 fname = dir('*.mat');
-[tot_count,stage_count,flat_count,norm_count] = find_count(fname,t_event); 
-[~,niu_count,niu_flat_count,niu_norm_count] = find_count(fname,t_intersec_event);
-sprintf('Among the %d breaths analyzed, %d are in stage specified by event_time, %d are flattened, %d are normal',tot_count,stage_count,flat_count,norm_count)
-sprintf('Among the %d in apnea/hypopnea within that stage, %d are flattened and %d are normal',niu_count,niu_flat_count,niu_norm_count)
+[tot_count,stage_count,flat_count,norm_count,ls_count] = find_count(fname,t_event); 
+[~,niu_count,niu_flat_count,niu_norm_count,niu_ls_count] = find_count(fname,t_intersec_event);
+sprintf('Among the %d breaths analyzed, %d are in stage specified by event_time, %d are flattened, %d are normal, %d are low signal',tot_count,stage_count,flat_count,norm_count,ls_count)
+sprintf('Among the %d in apnea/hypopnea within that stage, %d are flattened and %d are normal,%d are low signal',niu_count,niu_flat_count,niu_norm_count,niu_ls_count)
 end
 

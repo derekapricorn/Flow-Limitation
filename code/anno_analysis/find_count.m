2cba4e8272651c22f52079adcdec4d35abd839b0
Derek Zhi <derek.zhi@mail.utoronto.ca>
2015-03-24 12:00:34 -0400

    find_count

--- code/anno_analysis/find_count.m
+++ code/anno_analysis/find_count.m
@@ -8,31 +8,33 @@ stage_count = 0;
 flat_count = 0;
 norm_count = 0;
 for jj = 1:length(fname)
-    load(fname(jj).name);
-    for kk = 1:length(t_cell)
-        temp = t_cell{kk}(1);%take only the start time
-        %check if the breath under analysis is in the stage of interest
-        flag = any(temp > t_event(:,1) & temp < t_event(:,2));
-        if flag 
-            if strcmp(type_cell{kk},'Flattened')||strcmp(type_cell{kk},'Low signal')
-                flat_count = flat_count + 1;    
-%             elseif strcmp(type_cell{kk},'Intermediate')
-%                 int_count = int_count + 1;
-            elseif strcmp(type_cell{kk},'Normal')
-                norm_count = norm_count + 1;
-            elseif strcmp(type_cell{kk},'Unknown') %in case of Unknown, neglect the breath
-                
-            %in newer version e.g. ss26 the name has to be changed
- %           elseif strcmp(type_cell{kk},'Obstructive Hypopnea w/ Arousal')
-                %
-            
-                tot_count = tot_count - 1;
-                stage_count = stage_count -1;
-            end
-            stage_count = stage_count + 1;
-        end       
-     end
-     tot_count = tot_count + length(t_cell);
+    if ~strcmp(fname(jj).name,'event_time.mat')
+        load(fname(jj).name);
+        for kk = 1:length(t_cell)
+            temp = t_cell{kk}(1);%take only the start time
+            %check if the breath under analysis is in the stage of interest
+            flag = any(temp > t_event(:,1) & temp < t_event(:,2));
+            if flag 
+                if strcmp(type_cell{kk},'Flattened')||strcmp(type_cell{kk},'Low signal')
+                    flat_count = flat_count + 1;    
+    %             elseif strcmp(type_cell{kk},'Intermediate')
+    %                 int_count = int_count + 1;
+                elseif strcmp(type_cell{kk},'Normal')
+                    norm_count = norm_count + 1;
+                elseif strcmp(type_cell{kk},'Unknown') %in case of Unknown, neglect the breath
+
+                %in newer version e.g. ss26 the name has to be changed
+     %           elseif strcmp(type_cell{kk},'Obstructive Hypopnea w/ Arousal')
+                    %
+
+                    tot_count = tot_count - 1;
+                    stage_count = stage_count -1;
+                end
+                stage_count = stage_count + 1;
+            end       
+         end
+         tot_count = tot_count + length(t_cell);
+    end
 end
 
 
